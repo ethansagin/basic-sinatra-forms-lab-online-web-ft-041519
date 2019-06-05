598fdb05bbc6de731c1ba8912ed968d767c3f079
Ethan Sagin <ethansagin@gmail.com>
2019-06-05 14:52:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,15 +7,16 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @name = params["name"]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @sf = params[:sf]
-    @pf = params[:pf]
-    @c = params[:c]
+    "Hello"
+    # @name = params["name"]
+    # @coach = params[:coach]
+    # @pg = params[:pg]
+    # @sg = params[:sg]
+    # @sf = params[:sf]
+    # @pf = params[:pf]
+    # @c = params[:c]
     
-    erb :team
+    # erb :team
   end
 
 end
